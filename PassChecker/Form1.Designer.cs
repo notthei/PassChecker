be3931ac4f31fa5e2f5a56723de77714829e614c
のってぃ <74302768+notthei@users.noreply.github.com>
2024-11-01 01:59:28 +0900

    FirstCommit

---
+++ PassChecker/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace PassChecker
+{
+    partial class PasswordChecker
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(24, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 41);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "ここにパスワードを入力";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(24, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(311, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelResult.Location = new System.Drawing.Point(3, 44);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(196, 27);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.Text = "                       ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(3, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "                ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.labelResult);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(24, 174);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(311, 252);
+            this.panel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(130, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "結果";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(349, 174);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(416, 252);
+            this.panel2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(13, 77);
+            this.label4.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(403, 108);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0～1点 : 弱いパスワードです。\r\n2点 : 普通のパスワードです。\r\n3点 : 強力なパスワードです。\r\n4点 : 非常に強力なパスワードです！";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(125, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "スコアについて";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 5;
+            // 
+            // PasswordChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "PasswordChecker";
+            this.Text = "PasswordChecker";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PassChecker/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassChecker
+{
+    public partial class PasswordChecker : Form
+    {
+        public PasswordChecker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = textBox1.Text;
+            string strength = CheckPasswordStrength(password);
+            labelResult.Text = strength;
+        }
+        private string CheckPasswordStrength(string password)
+        {
+          
+            int score = 0;
+            int charSetSize = 0;
+
+            if (Regex.IsMatch(password, @"[A-Z]")) { score++; charSetSize += 26; } // 大文字
+            if (Regex.IsMatch(password, @"[a-z]")) { score++; charSetSize += 26; } // 小文字
+            if (Regex.IsMatch(password, @"[0-9]")) { score++; charSetSize += 10; } // 数字
+            if (Regex.IsMatch(password, @"[!@#$%^&*()_+=-]")) { score++; charSetSize += 32; } // 特殊文字
+
+            string strengthMessage;
+            tCalculation(password, charSetSize);
+            // パスワードの強度評価
+
+            strengthMessage = "スコア:"+score.ToString();
+            return strengthMessage;
+
+        }
+
+        private void tCalculation(string st , int cha )
+        {
+
+            // 総当たり時間の計算
+            double totalCombinations = Math.Pow(cha, st.Length);
+            double secondsToCrack = totalCombinations / 1000000; // 1秒間に100万回試行
+            double minutesToCrack = secondsToCrack / 60;
+
+            string timeMessage = minutesToCrack < 1
+                ? $"{Math.Round(secondsToCrack, 2)}秒"
+                : $"{Math.Round(minutesToCrack, 2)}分";
+
+           label1.Text ="解析にかかる時間 \n約"+ timeMessage;
+            label5.Text = "※総当たりの場合";
+        }
+
+    }
+}
